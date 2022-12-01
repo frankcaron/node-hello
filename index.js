7b080bce9690c9dcdded4cbfbcb30777357825c7
Frank Caron <frank@frankcaron.com>
2022-12-01 23:15:55 +0000

    Fixed issue forcing download do to content type.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-
+/
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
 
 server.listen(port, () => {
